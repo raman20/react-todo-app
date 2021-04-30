ea85d021f611e0b61d0f8e4d4e524655876eb50b
raman20 <ramandwivedi20@protonmail.com>
2021-04-30 17:49:02 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class TodoListRow extends React.Component {
+	render() {
+		const row = this.props.tasks.map((item, index) => {
+			return (
+				<tr key={index}>
+					<td>{item}	<button onClick={() => {this.props.deleteTasks(index);}}>delete</button></td>
+				</tr>
+			);
+		});
+		return <tbody>{row}</tbody>;
+	}
+}
+class TodoList extends React.Component {
+	render() {
+		return (
+			<table>
+				<TodoListRow tasks={this.props.tasks} deleteTasks={this.props.deleteTasks}/>
+			</table>
+		);
+	}
+}
+class TodoInput extends React.Component {
+	add = () => {
+		let inp = document.getElementById("inp");
+		this.props.onAdd(inp.value);
+		inp.value = "";
+	};
+	render() {
+		return (
+			<div className="Input">
+				<input type="text" id="inp" placeholder="enter task todo" />
+				<button onClick={this.add}>Add</button>
+			</div>
+		);
+	}
+}
+class TodoApp extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { items: [] };
+	}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	addTasks = (tasks) => {
+		this.setState({items: this.state.items.concat(tasks)});
+	};
+
+	deleteTasks = (index) => {
+		let tempItems = this.state.items;
+		tempItems = tempItems.filter((item,idx)=>{
+			return idx !== index;
+		})
+		this.setState({items: tempItems});
+	};
+
+	render() {
+		return (
+			<div className="app">
+				<h1>Todo App</h1>
+				<TodoInput onAdd={this.addTasks} />
+				<br/>
+				<TodoList tasks={this.state.items} deleteTasks = {this.deleteTasks}/>
+			</div>
+		);
+	}
+}
+
+
+ReactDOM.render(<TodoApp />, document.getElementById("root"));
