e5c32ce2ea028457adb8b9736c2e7368a6e926e2
raman20 <ramandwivedi20@protonmail.com>
2021-05-30 00:04:33 +0530

    structure changed

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,93 +1,6 @@
 import './App.css';
 import React from 'react';
 
-class Button extends React.Component {
-  render() {
-    return (
-      <Context.Consumer>
-        {
-          (obj) => {
-            return <button onClick={(_e) => { obj.input(this.props.val) }}>{this.props.val}</button>
-          }
-        }
-      </Context.Consumer>
-    );
-  }
-}
-
-class SpecialButton extends React.Component {
-  render() {
-    return (
-      <Context.Consumer>
-        {
-          (obj) => {
-            let inpVal;
-            if (this.props.val === 'C') {
-              inpVal = "";
-            }
-            else {
-              try {
-                // eslint-disable-next-line no-eval
-                inpVal = eval(obj.dispVal);
-              }
-              catch (e) {
-                inpVal = 'error'
-              }
-            }
-            return (<button onClick={(_e) => { obj.updateDisp(inpVal) }}>{this.props.val}</button>)
-          }}
-      </Context.Consumer>
-    )
-  }
-
-}
-
-
-class ButtonSpace extends React.Component {
-  render() {
-    return (
-      <table>
-        <thead></thead>
-        <tbody>
-          <tr>
-            <td><Button val={'1'} /></td>
-            <td><Button val={'2'} /></td>
-            <td><Button val={'3'} /></td>
-            <td><Button val={'+'} /></td>
-          </tr>
-          <tr>
-            <td><Button val={'4'} /></td>
-            <td><Button val={'5'} /></td>
-            <td><Button val={'6'} /></td>
-            <td><Button val={'-'} /></td>
-          </tr>
-          <tr>
-            <td><Button val={'7'} /></td>
-            <td><Button val={'8'} /></td>
-            <td><Button val={'9'} /></td>
-            <td><Button val={'*'} /></td>
-          </tr>
-          <tr>
-            <td><Button val={0} /></td>
-            <td><SpecialButton val={'C'} /></td>
-            <td><SpecialButton val={'='} /></td>
-            <td><Button val={'/'} /></td>
-          </tr>
-        </tbody>
-        <tfoot></tfoot>
-      </table>
-    );
-  }
-}
-
-class Screen extends React.Component {
-  render() {
-    return (
-      <input type="text" value={this.props.val} disabled />
-    );
-  }
-}
-
 const Context = React.createContext();
 
 class App extends React.Component {
@@ -134,6 +47,91 @@ class App extends React.Component {
     );
   }
 
+}
+
+class Screen extends React.Component {
+  render() {
+    return (
+      <input type="text" value={this.props.val} disabled />
+    );
+  }
+}
+
+class ButtonSpace extends React.Component {
+  render() {
+    return (
+      <table>
+        <thead></thead>
+        <tbody>
+          <tr>
+            <td><Button val={'1'} /></td>
+            <td><Button val={'2'} /></td>
+            <td><Button val={'3'} /></td>
+            <td><Button val={'+'} /></td>
+          </tr>
+          <tr>
+            <td><Button val={'4'} /></td>
+            <td><Button val={'5'} /></td>
+            <td><Button val={'6'} /></td>
+            <td><Button val={'-'} /></td>
+          </tr>
+          <tr>
+            <td><Button val={'7'} /></td>
+            <td><Button val={'8'} /></td>
+            <td><Button val={'9'} /></td>
+            <td><Button val={'*'} /></td>
+          </tr>
+          <tr>
+            <td><Button val={0} /></td>
+            <td><SpecialButton val={'C'} /></td>
+            <td><SpecialButton val={'='} /></td>
+            <td><Button val={'/'} /></td>
+          </tr>
+        </tbody>
+        <tfoot></tfoot>
+      </table>
+    );
+  }
+}
+
+class Button extends React.Component {
+  render() {
+    return (
+      <Context.Consumer>
+        {
+          (obj) => {
+            return <button onClick={(_e) => { obj.input(this.props.val) }}>{this.props.val}</button>
+          }
+        }
+      </Context.Consumer>
+    );
+  }
+}
+
+class SpecialButton extends React.Component {
+  render() {
+    return (
+      <Context.Consumer>
+        {
+          (obj) => {
+            let inpVal;
+            if (this.props.val === 'C') {
+              inpVal = "";
+            }
+            else {
+              try {
+                // eslint-disable-next-line no-eval
+                inpVal = eval(obj.dispVal);
+              }
+              catch (e) {
+                inpVal = 'error'
+              }
+            }
+            return (<button onClick={(_e) => { obj.updateDisp(inpVal) }}>{this.props.val}</button>)
+          }}
+      </Context.Consumer>
+    )
+  }
 }
 
 export default App;
